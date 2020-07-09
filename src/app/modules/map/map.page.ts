65281d55a3f88a215637d1e5033697f29fff0f68
Julian Narvaez <oscarnarvaez01@hotmail.com>
2020-07-09 03:09:33 -0500

    maps working

--- src/app/modules/map/map.page.ts
+++ src/app/modules/map/map.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChild,ElementRef } from '@angular/core';
 
+declare var google:any;
 @Component({
   selector: 'app-map',
   templateUrl: './map.page.html',
@@ -7,9 +9,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapPage implements OnInit {
 
+  map:any;
+  @ViewChild('map',{read:ElementRef,static:false})mapRef:ElementRef;
   constructor() { }
 
   ngOnInit() {
+  }
+  ionViewDidEnter(){
+    this.showmap();
+  }
+  infoWindows:any=[];
+  marker:any=[
+    {
+      title:"hola",
+      latitude:"4.571136",
+      longitude:"-74.21952"
+    }
+  ]
+
+  addMarkersToMap(markers){
+    for(let marker of markers){
+      let position= new google.maps.LatLng(marker.latitude,marker.longitude);
+      let mapMarker = new google.maps.Marker({
+        position:position,
+        title:marker.title,
+        latitude:marker.latitude,
+        longitude:marker.longitude
+      });
+      
+      marker.setMap(this.map);
+      this.addInfoWindowToMarker(mapMarker);
+    }
+  }
+
+  addInfoWindowToMarker(marker){
+    let infoWindowContent= '<div id="content">'+
+                              '<h2 id="firstHeading" class="firstHeading">'+marker.title+'</h2>'+
+                              '<p>Latitude:'+marker.latitude+'</p>'+
+                              '<p>Longitud:'+marker.longitude+'</p>'+
+                              '</div>';
+    let infoWindow = new google.maps.infoWindow({
+      content:infoWindowContent
+    });
+    marker.addListener('click',()=>{
+      this.closeAllInfoWindows();
+      infoWindow.open(this.map, marker);
+    });
+    this.infoWindows.push(infoWindow);
+  }
+
+  closeAllInfoWindows(){
+    for(let window of this.infoWindows){
+      window.close();
+    }
+  }
+
+
+  showmap(){
+    const location = new google.maps.LatLng(4.571136,-74.21952)
+    const options = {
+      center:location,
+      zoom:15,
+      disableDefaultUI:true
+    }
+    this.map=new google.maps.Map(this.mapRef.nativeElement,options);
   }
 
 }
